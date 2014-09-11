760aa54809b4fabe436e70b3f28455dfae0159fa
Joshua Pham <joshua.pham@gmail.com>
2014-09-11 15:56:19 -0700

    bye scrapy. i'm in love with goose.

---
+++ webscraper/article_extractor.py
@@ -0,0 +1,6 @@
+from goose import Goose
+url = "http://www.theverge.com/2014/9/11/6136443/the-largest-predatory-dinosaur-ever-was-half-duck-half-crocodile"
+g = Goose()
+article = g.extract(url=url)
+print article.title
+print article.cleaned_text
