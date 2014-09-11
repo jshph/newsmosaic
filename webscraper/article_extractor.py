acecd7c78e6bbd58d219ec963ad4b777d48b35b6
Joshua Pham <joshua.pham@gmail.com>
2014-09-11 16:52:42 -0700

    oops

--- webscraper/article_extractor.py
+++ webscraper/article_extractor.py
@@ -1,3 +1,5 @@
+# install Goose https://github.com/grangier/python-goose
+
 from goose import Goose
 url = "http://www.theverge.com/2014/9/11/6136443/the-largest-predatory-dinosaur-ever-was-half-duck-half-crocodile"
 g = Goose()
