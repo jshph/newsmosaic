bdf69fd1b33f9dc0d9ad3eb5364ef37faf9b91fa
duc <ducnguy@stanford.edu>
2014-09-24 20:01:43 -0700

    added before/after request methods

--- app/routes.py
+++ app/routes.py
@@ -28,6 +28,16 @@ def init_db():
     db.commit()
     print "created"
 
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+
 def hello():
 	print "hello"
 
