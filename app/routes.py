2aec264a45a02bfc650dff7541c52e680afac738
Joshua Pham <joshua.pham@gmail.com>
2014-09-24 19:54:20 -0700

    added init_db (and not sure if the db file went thru as well)

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 from urllib2 import Request, urlopen, URLError
+from contextlib import closing
 import threading
 import datasource
 import nltk
@@ -19,6 +20,13 @@ app.config.from_object(__name__)
 
 def connect_db():
 	return sqlite3.connect(app.config['DATABASE']) 
+
+def init_db():
+  with closing(connect_db()) as db:
+    with app.open_resource('static/schema.sql', mode='r') as f:
+      db.cursor().executescript(f.read())
+    db.commit()
+    print "created"
 
 def hello():
 	print "hello"
