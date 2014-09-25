1e16e4ad4a864d590dc2877e84ab727059d7c815
duc <ducnguy@stanford.edu>
2014-09-24 20:01:43 -0700

    added before/after request methods
    Conflicts:
    	app/routes.py

--- app/routes.py
+++ app/routes.py
@@ -20,6 +20,26 @@ app.config.from_object(__name__)
 def connect_db():
 	return sqlite3.connect(app.config['DATABASE']) 
 
+<<<<<<< HEAD
+=======
+def init_db():
+  with closing(connect_db()) as db:
+    with app.open_resource('static/schema.sql', mode='r') as f:
+      db.cursor().executescript(f.read())
+    db.commit()
+    print "created"
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+
+>>>>>>> bdf69fd... added before/after request methods
 def hello():
 	print "hello"
 
