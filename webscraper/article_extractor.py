7f149e2cc516debced50ed346925801a9a1f274c
smerdy <joshua.pham@gmail.com>
2016-01-19 23:22:09 -0600

    use topia on example verge article. result is dirty

--- webscraper/article_extractor.py
+++ webscraper/article_extractor.py
@@ -1,8 +1,14 @@
 # install Goose https://github.com/grangier/python-goose
 
 from goose import Goose
+from topia.termextract import extract
+extractor = extract.TermExtractor()
+
 url = "http://www.theverge.com/2014/9/11/6136443/the-largest-predatory-dinosaur-ever-was-half-duck-half-crocodile"
 g = Goose()
 article = g.extract(url=url)
-print article.title
-print article.cleaned_text
+# print article.title
+# print article.cleaned_text
+x = sorted(extractor(article.cleaned_text))
+for i in x:
+	print i[0]
